c23fb557ef8d079e2982c50b5a2256dab1773bc6
DESKTOP-FSNB2S3\dan_s <dan.sabadas@gmail.com>
2017-10-03 21:08:32 +0300

    added minimal modal dialog with mapping with context

--- knockout-fundamentals/App_Start/BundleConfig.cs
+++ knockout-fundamentals/App_Start/BundleConfig.cs
@@ -10,6 +10,9 @@ namespace knockout_fundamentals
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
--- knockout-fundamentals/Scripts/bp-index.js
+++ knockout-fundamentals/Scripts/bp-index.js
@@ -1,27 +1,43 @@
-﻿/// <reference path="~/Scripts/knockout-3.4.2.debug.js" />
+﻿/// <reference path="knockout-3.4.2.debug.js" />
 /// <reference path="jquery-1.10.2.js" />
 
 $(function () {
-  //$("#tagDialog").hide();
+        $("#tagDialog").hide();
+            var data = [
+            { Id: 1, Name: "Ball Handling" },
+            { Id: 2, Name: "Passing" },
+            { Id: 3, Name: "Shooting" },
+            { Id: 4, Name: "Rebounding" },
+            { Id: 5, Name: "Transition" },
+            { Id: 6, Name: "Defense" },
+            { Id: 7, Name: "Team Offense" },
+            { Id: 8, Name: "Team Defense" }
+        ];
+        //  var data = [
+        //      new tagItem(1, "Ball Handling"),
+        //      new tagItem(2, "Passing"),
+        //      new tagItem(3, "Shooting"),
+        //      new tagItem(4, "Rebounding"),
+        //      new tagItem(5, "Transition"),
+        //      new tagItem(6, "Defense"),
+        //      new tagItem(7, "Team Offense"),
+        //      new tagItem(8, "Team Defense")
+        //];
 
-          var data = [
-              { Id: 1, Name: "Ball Handling" },
-              { Id: 2, Name: "Passing" },
-              { Id: 3, Name: "Shooting" },
-              { Id: 4, Name: "Rebounding" },
-              { Id: 5, Name: "Transition" },
-              { Id: 6, Name: "Defense" },
-              { Id: 7, Name: "Team Offense" },
-              { Id: 8, Name: "Team Defense" }
-          ];
           var viewModel = {
             tags: ko.observableArray(data),
             tagToAdd: ko.observable(""),
+            selectedTag: ko.observable(null),
+
             addTag: function() {
               var newTag = { Name: this.tagToAdd() };
               this.tags.push(newTag);
               this.tagToAdd("");
-            }
+            },
+            selectTag: function () {
+                viewModel.selectedTag(this);
+                console.log(this);
+              }
           };
 
           $(document).on("click", ".tag-delete", function () {
@@ -29,14 +45,26 @@ $(function () {
             viewModel.tags.remove(item);
           });
 
+          $(document).on("click", ".tag-edit", function () {
+              $("#tagDialog").dialog({
+                  buttons: {
+                      Save: function () {
+                          //viewModel.selectedTag().Name.commit();
+                          $(this).dialog("close");
+                      },
+                      Cancel: function () { $(this).dialog("close"); }
+                  }
+              });
+          });
+            
           ko.applyBindings(viewModel);
 
-  //    function tagItem(name, id) {
-  //        return {
-  //            Name: ko.protectedObservable(name),
-  //            Id: ko.protectedObservable(id)
-  //        };
-  //    }
+      function tagItem(id, name) {
+          return {
+              Name: ko.observable(name),
+              Id: ko.observable(id)
+          };
+      }
 
   //$.getJSON("/tags", function (data) {
   //  var viewModel = {
